5e02721dac9443ed1ed099b8b4ed29af48146aaf
Mark Janssen <mark@praseodym.net>
2014-08-27 16:24:42 +0200

    PKIapplet card init

--- src/libopensc/card-pkiapplet.c
+++ src/libopensc/card-pkiapplet.c
@@ -1,5 +1,5 @@
 /*
- * card-pkiappet.c: Support for PKIapplet cards.
+ * card-pkiappet.c: Support for PKIapplet (javacardsign applet) cards.
  *
  * Copyright (C) 2014 Mark Janssen <mark@praseodym.net>
  *
@@ -34,7 +34,7 @@ static struct sc_atr_table pkiapplet_atrs[] = {
 static struct sc_card_operations *iso_ops;
 static struct sc_card_operations pkiapplet_ops;
 static struct sc_card_driver pkiapplet_drv = {
-	"PKIapplet card",
+	"PKIapplet (javacardsign applet) card",
 	"pkiapplet",
 	&pkiapplet_ops,
 	NULL, 0, NULL
@@ -43,6 +43,7 @@ static struct sc_card_driver pkiapplet_drv = {
 static int pkiapplet_match_card(sc_card_t * card)
 {
 	int i;
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 
 	i = _sc_match_atr(card, pkiapplet_atrs, &card->type);
 	if (i < 0)
@@ -50,183 +51,75 @@ static int pkiapplet_match_card(sc_card_t * card)
 	return 1;
 }
 
+static int select_pkcs15_app(sc_card_t * card)
+{
+	sc_path_t app;
+	int r;
+
+	/* Regular PKCS#15 AID */
+	sc_format_path("A000000063504B43532D3135", &app);
+	app.type = SC_PATH_TYPE_DF_NAME;
+	r = sc_select_file(card, &app, NULL);
+	if (r != SC_SUCCESS) {
+		sc_debug(card->ctx, SC_LOG_DEBUG_NORMAL, "unable to select PKCS15 applet");
+		return r;
+	}
+
+	return SC_SUCCESS;
+}
+
 static int pkiapplet_init(sc_card_t * card)
 {
-	card->max_recv_size = 128;
-	card->max_send_size = 128;
-	return SC_SUCCESS;
-}
-
-static int pkiapplet_select_file_by_path(sc_card_t * card,
-				     const sc_path_t * in_path,
-				     sc_file_t ** file_out)
-{
-	int in_len = in_path->len;
-	const u8 *in_pos = in_path->value;
-	sc_path_t path;
-
-	memset(&path, 0, sizeof(sc_path_t));
-	path.len = 2;		/* one component at a time */
-	path.type = SC_PATH_TYPE_FILE_ID;
-
-	/*
-	 * Check parameters.
-	 */
-	if (in_len % 2 != 0)
-		return SC_ERROR_INVALID_ARGUMENTS;
-
-	/*
-	 * File ID by file ID...
-	 */
-	while (in_len) {
-		int result;
-		memcpy(path.value, in_pos, 2);
-		result = iso_ops->select_file(card, &path, file_out);
-		if (result != SC_SUCCESS)
-			return result;
-		in_len -= 2;
-		in_pos += 2;
-	}
-	return SC_SUCCESS;
-}
-
-static int pkiapplet_select_file(sc_card_t * card,
-			     const sc_path_t * in_path, sc_file_t ** file_out)
-{
-	switch (in_path->type) {
-
-	case SC_PATH_TYPE_PATH:
-		return pkiapplet_select_file_by_path(card, in_path, file_out);
-
-	default:
-		return iso_ops->select_file(card, in_path, file_out);
-	}
-}
-
-static int pkiapplet_get_serialnr(sc_card_t * card, sc_serial_number_t * serial)
-{
+	unsigned int flags;
 	int r;
-	u8 rbuf[SC_MAX_APDU_BUFFER_SIZE];
-	sc_apdu_t apdu;
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
 
-	/*
-	 * Check arguments.
-	 */
-	if (!serial)
-		return SC_ERROR_INVALID_ARGUMENTS;
+	card->name = "pkiapplet";
+	card->cla  = 0x00;
+	card->drv_data = NULL;
 
-	/*
-	 * Return a cached serial number, if we have one.
-	 */
-	if (card->serialnr.len) {
-		memcpy(serial, &card->serialnr, sizeof(*serial));
-		return SC_SUCCESS;
-	}
+	flags =SC_ALGORITHM_ONBOARD_KEY_GEN
+		 | SC_ALGORITHM_RSA_RAW
+		 | SC_ALGORITHM_RSA_HASH_NONE;
 
-	/*
-	 * Fetch serial number using GET CARD INFO.
-	 */
-	sc_format_apdu(card, &apdu, SC_APDU_CASE_2_SHORT, 0x14, 0, 0);
-	apdu.cla |= 0x80;
-	apdu.resp = rbuf;
-	apdu.resplen = sizeof(rbuf);
-	apdu.le = 6;
-	r = sc_transmit_apdu(card, &apdu);
-	SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
-	if (apdu.sw1 != 0x90 || apdu.sw2 != 0x00)
-		return SC_ERROR_INTERNAL;
+	_sc_card_add_rsa_alg(card, 1024, flags, 0);
 
-	/*
-	 * Cache serial number.
-	 */
-	memcpy(card->serialnr.value, apdu.resp, MIN(apdu.resplen, SC_MAX_SERIALNR));
-	card->serialnr.len = MIN(apdu.resplen, SC_MAX_SERIALNR);
+	card->caps = SC_CARD_CAP_RNG;
 
-	/*
-	 * Copy and return serial number.
-	 */
-	memcpy(serial, &card->serialnr, sizeof(*serial));
-	return SC_SUCCESS;
-}
+	/* we need read_binary&friends with max 224 bytes per read */
+	card->max_send_size = 255;
+	card->max_recv_size = 255;
 
-static int pkiapplet_card_ctl(sc_card_t * card, unsigned long cmd, void *ptr)
-{
-	switch (cmd) {
-
-	case SC_CARDCTL_GET_SERIALNR:
-		return pkiapplet_get_serialnr(card, (sc_serial_number_t *) ptr);
-
-	default:
-		return SC_ERROR_NOT_SUPPORTED;
-	}
-}
-
-static int pkiapplet_list_files(sc_card_t * card, u8 * buf, size_t buflen)
-{
-	sc_apdu_t apdu;
-	int r;
-	size_t count;
-	u8 *bufp = buf;		/* pointer into buf */
-	int fno = 0;		/* current file index */
-
-	/*
-	 * Check parameters.
-	 */
-	if (!buf || (buflen & 1))
-		return SC_ERROR_INVALID_ARGUMENTS;
-
-	/*
-	 * Use CARD GET INFO to fetch the number of files under the
-	 * curently selected DF.
-	 */
-	sc_format_apdu(card, &apdu, SC_APDU_CASE_1, 0x14, 0x01, 0x00);
-	apdu.cla |= 0x80;
-	r = sc_transmit_apdu(card, &apdu);
-	SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
-	if (apdu.sw1 != 0x90)
-		return SC_ERROR_INTERNAL;
-	count = apdu.sw2;
-
-	while (count--) {
-		u8 info[8];
-
-		/*
-		 * Truncate the scan if no more room left in output buffer.
-		 */
-		if (buflen == 0)
-			break;
-
-		sc_format_apdu(card, &apdu, SC_APDU_CASE_2_SHORT, 0x14, 0x02,
-			       fno++);
-		apdu.cla |= 0x80;
-		apdu.resp = info;
-		apdu.resplen = sizeof(info);
-		apdu.le = sizeof(info);
-		r = sc_transmit_apdu(card, &apdu);
-		SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
-		if (apdu.sw1 != 0x90 || apdu.sw2 != 0x00)
-			return SC_ERROR_INTERNAL;
-
-		*bufp++ = info[2];
-		*bufp++ = info[3];
-		buflen -= 2;
-	}
-
-	return (bufp - buf);
+	r = select_pkcs15_app(card);
+	SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_VERBOSE, r);
 }
 
 static struct sc_card_driver *sc_get_driver(void)
 {
 	struct sc_card_driver *iso_drv = sc_get_iso7816_driver();
 
-	iso_ops = iso_drv->ops;
-	pkiapplet_ops = *iso_ops;
+	if (iso_ops == NULL)
+		iso_ops = iso_drv->ops;
 
+	pkiapplet_ops = *iso_ops;
 	pkiapplet_ops.match_card = pkiapplet_match_card;
 	pkiapplet_ops.init = pkiapplet_init;
-	pkiapplet_ops.select_file = pkiapplet_select_file;
-	pkiapplet_ops.card_ctl = pkiapplet_card_ctl;
-	pkiapplet_ops.list_files = pkiapplet_list_files;
+
+#if 0
+    /* iso7816-4 functions */
+    pkiapplet_ops.read_binary   = pkiapplet_read_binary;
+    pkiapplet_ops.write_binary  = NULL;
+    pkiapplet_ops.update_binary = pkiapplet_update_binary;
+
+    /* iso7816-8 functions */
+    pkiapplet_ops.restore_security_env = pkiapplet_restore_security_env;
+    pkiapplet_ops.set_security_env  = pkiapplet_set_security_env;
+    pkiapplet_ops.decipher = pkiapplet_decipher;
+    pkiapplet_ops.compute_signature = pkiapplet_compute_signature;
+
+    /* iso7816-9 functions */
+    pkiapplet_ops.pin_cmd = pkiapplet_pin_cmd;
+#endif
 
 	return &pkiapplet_drv;
 }
